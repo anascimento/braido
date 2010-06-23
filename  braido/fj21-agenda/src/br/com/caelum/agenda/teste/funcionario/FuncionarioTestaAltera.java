80e002b76c2946bbdba4d81996b3bd726502b6b9
bruno.braido <bruno.braido@06c6c6a8-f44a-11dd-afc9-ff9d66c6243e>
2010-06-23 01:34:28 +0000

    Corrigido a pasta agenda.

---
+++  braido/fj21-agenda/src/br/com/caelum/agenda/teste/funcionario/FuncionarioTestaAltera.java
@@ -0,0 +1,24 @@
+package br.com.caelum.agenda.teste.funcionario;
+
+import br.com.caelum.agenda.dao.FuncionarioDAO;
+import br.com.caelum.agenda.modelo.Funcionario;
+
+public class FuncionarioTestaAltera {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//Setando os valores a serem salvos
+		Funcionario funcionario = new Funcionario();
+		funcionario.setId(2);
+		funcionario.setNome("Caelum 2");
+		funcionario.setUsuario("caelum2");
+		funcionario.setSenha("pwdcaelum2");
+		
+		//Instanciando para gravar no Banco através do DAO
+		FuncionarioDAO dao = new FuncionarioDAO();
+		dao.altera(funcionario);
+		System.out.println("Dados alterados!");
+	}
+}
