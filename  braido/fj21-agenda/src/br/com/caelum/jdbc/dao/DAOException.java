c436a7331dae7627b4b35fe10ccf59f476490a71
bruno.braido <bruno.braido@06c6c6a8-f44a-11dd-afc9-ff9d66c6243e>
2010-06-23 01:36:47 +0000

    Removido a pasta jdbc

---  braido/fj21-agenda/src/br/com/caelum/jdbc/dao/DAOException.java
+++
@@ -1,9 +0,0 @@
-package br.com.caelum.agenda.dao;
-
-import java.sql.SQLException;
-
-public class DAOException extends RuntimeException {
-	public void getException(Exception e){
-		System.out.println(e);
-	}
-}
